2118e72296ca316836e0dec7ec2050942a332be3
Paresh-Kalsotra <pareshkalsotra@gmail.com>
2024-03-26 13:54:04 +0530

    quiz app update

--- frontend/src/components/QuestionCard.jsx
+++ frontend/src/components/QuestionCard.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const server_uri = import.meta.env.VITE_SERVER_URI;
 
 const QuestionCard = () => {
   const questions = useSelector((store) => store.quiz.quizQuestions);
   const [userResponses, setUserResponses] = useState([]);
   const [quesNo, setQuesNo] = useState(1);
-
+  const [score, setScore] = useState(null);
   const question = questions[quesNo - 1]; //getting question based on question number
+
+  const navigate = useNavigate();
 
   function handlePrev() {
     setQuesNo((q) => q - 1);
@@ -40,64 +43,83 @@ const QuestionCard = () => {
 
     const res = await response.json();
 
-    alert("Your Score is " + res + " out of 5");
+    setScore(res);
+  }
+
+  function handleReturn() {
+    navigate("/home");
   }
 
   return (
     <>
-      <h3>
-        {quesNo}. {question.title}
-      </h3>
-      {question.choices.map((item) => (
-        <div key={item.id}>
-          <button
-            className={
-              userResponses.some(
-                (response) =>
-                  response._id === question._id && response.answer === item.id
-              )
-                ? "activeSelection"
-                : "passiveSelection"
-            }
-            onClick={() => handleSelection(item)}
-          >
-            {item.content}
-          </button>
+      {score ? (
+        <p>
+          Your Score: {score}/5
           <br />
-        </div>
-      ))}
-
-      <hr />
-      {quesNo > 1 ? (
-        <button
-          className="quesbtn"
-          style={{ float: "left" }}
-          onClick={handlePrev}
-        >
-          Prev
-        </button>
+          Go back to Home to attempt another quiz.
+          <br />
+          <button className="homeReturnBtn" onClick={handleReturn}>
+            Home
+          </button>
+        </p>
       ) : (
-        <></>
-      )}
+        <>
+          <h3>
+            {quesNo}. {question.title}
+          </h3>
+          {question.choices.map((item) => (
+            <div key={item.id}>
+              <button
+                className={
+                  userResponses.some(
+                    (response) =>
+                      response._id === question._id &&
+                      response.answer === item.id
+                  )
+                    ? "activeSelection"
+                    : "passiveSelection"
+                }
+                onClick={() => handleSelection(item)}
+              >
+                {item.content}
+              </button>
+              <br />
+            </div>
+          ))}
 
-      <button
-        className="quesbtn"
-        style={{ marginLeft: "30%" }}
-        onClick={handleSubmit}
-      >
-        Submit
-      </button>
+          <hr />
+          {quesNo > 1 ? (
+            <button
+              className="quesbtn"
+              style={{ float: "left" }}
+              onClick={handlePrev}
+            >
+              Prev
+            </button>
+          ) : (
+            <></>
+          )}
 
-      {quesNo < 5 ? (
-        <button
-          className="quesbtn"
-          style={{ float: "right" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      ) : (
-        <></>
+          <button
+            className="quesbtn"
+            style={{ marginLeft: quesNo > 1 ? "30%" : "45%" }}
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+
+          {quesNo < 5 ? (
+            <button
+              className="quesbtn"
+              style={{ float: "right" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          ) : (
+            <></>
+          )}
+        </>
       )}
     </>
   );
